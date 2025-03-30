2707e33e3413a85e906133e032a338479386731a
T.toshiki <daiamondtoshi@gmail.com>
2025-03-30 09:26:43 +0900

    追加修正

--- frontend/src/api/reportApi.ts
+++ frontend/src/api/reportApi.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 import { Reservation } from "@/types/reservation";
 
+const VITE_APP_REPORT_URL = import.meta.env.VITE_APP_REPORT_URL;
+
+
 export const sendCompletedReservations = async (reservations: Reservation[]) => {
-  return await axios.post("http://localhost:8000/api/report", {
+  return await axios.post(`${VITE_APP_REPORT_URL}`, {
     reservations,
   }, {
     headers: {
